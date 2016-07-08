07e5478dcc5a37bde70d4c3e6c0944e50f026518
Achilles Rasquinha <achillesrasquinha@gmail.com>
2016-07-08 22:01:53 +0530

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,11 @@ smoothScroll.init({
 $('#home-subtitle').typed({
     strings: ["developer.^1000 designer.^1000 dictator."]
 });
+
+$('.random-dark').each(function ( ) {
+    $(this).css('background-color', randomColor({ luminosity: 'dark' }));
+});
+
+$('.panel-toggle').click(function ( ) {
+    $(this).find('.fa-chevron').toggleClass('fa-chevron-up fa-chevron-down');
+});

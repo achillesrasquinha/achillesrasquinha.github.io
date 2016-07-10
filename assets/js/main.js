7aca827dc66dad8b606f870abf6b45aa01efe2ba
Achilles Rasquinha <achillesrasquinha@gmail.com>
2016-07-10 11:54:07 +0530

    changed social layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,30 @@
----
----
-smoothScroll.init({
-    speed    : 1000,
-    easing   : 'easeInOutQuint',
-    updateURL: false
-});
+$(document).ready(function ($) {
+  smoothScroll.init({
+      speed    : 1000,
+      easing   : 'easeInOutQuint',
+      updateURL: false
+  });
 
-$('#home-subtitle').typed({
-    strings: ["developer.^1000 designer.^1000 dictator."]
-});
+  $('#home-subtitle').typed({
+      strings: ["developer.^1000 designer.^1000 dictator."]
+  });
 
-$('.random-dark').each(function ( ) {
-    $(this).css('background-color', randomColor({ luminosity: 'dark' }));
-});
+  $('.random-dark').each(function ( ) {
+      $(this).css('background-color', randomColor({ luminosity: 'dark' }));
+  });
 
-$('.panel-toggle').click(function ( ) {
-    $(this).find('.fa-chevron').toggleClass('fa-chevron-up fa-chevron-down');
+  $('.panel-toggle').click(function ( ) {
+      $(this).find('.fa-chevron').toggleClass('fa-chevron-up fa-chevron-down');
+  });
+
+  $('.rrssb-buttons').each(function ( ) {
+    var url  = $(this).data('url');
+    var text = $(this).data('text');
+
+    $(this).rrssb({
+        title: text,
+        url  : url,
+    });
+  });
+
 });
